7347c668bd78ea00b21a4315273ae8869b136b5b
Colin <colin@mmin.us>
2022-06-10 19:13:09 +0800

    fix logo

--- frontend/src/metabase/public/components/LogoBadge.jsx
+++ frontend/src/metabase/public/components/LogoBadge.jsx
@@ -5,20 +5,7 @@ import LogoIcon from "metabase/components/LogoIcon";
 import ExternalLink from "metabase/core/components/ExternalLink";
 
 const LogoBadge = ({ dark }) => (
-  <ExternalLink
-    href="https://metabase.com/"
-    target="_blank"
-    className="h4 flex text-bold align-center no-decoration"
-  >
-    <LogoIcon height={28} dark={dark} />
-    <span className="text-small">
-      <span className="ml1 md-ml2 text-medium">{jt`Powered by ${(
-        <span className={dark ? "text-white" : "text-brand"}>
-          {t`Metabase`}
-        </span>
-      )}`}</span>
-    </span>
-  </ExternalLink>
+  <div></div>
 );
 
 export default LogoBadge;
