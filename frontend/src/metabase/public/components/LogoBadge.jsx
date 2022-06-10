f0300eb4eab6a3ab0248932efd050ca5c6a56948
Colin <colin@mmin.us>
2022-06-10 19:13:09 +0800

    fix logo

---
+++ frontend/src/metabase/public/components/LogoBadge.jsx
@@ -0,0 +1,11 @@
+/* eslint-disable react/prop-types */
+import React from "react";
+import { t, jt } from "ttag";
+import LogoIcon from "metabase/components/LogoIcon";
+import ExternalLink from "metabase/core/components/ExternalLink";
+
+const LogoBadge = ({ dark }) => (
+  <div></div>
+);
+
+export default LogoBadge;
