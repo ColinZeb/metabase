9b6c1314d9079010352dc0b2944ae7a11fca084a
ColinZeb <Colin@mmin.us>
2023-03-07 16:09:00 +0800

    hasPremiumFeature->true

--- enterprise/frontend/src/metabase-enterprise/settings/index.js
+++ enterprise/frontend/src/metabase-enterprise/settings/index.js
@@ -1,9 +1,5 @@
 import MetabaseSettings from "metabase/lib/settings";
 
 export function hasPremiumFeature(feature) {
-  const hasFeature = MetabaseSettings.get("token-features", {})[feature];
-  if (hasFeature == null) {
-    console.warn("Unknown premium feature", feature);
-  }
-  return hasFeature;
+  return true;
 }
