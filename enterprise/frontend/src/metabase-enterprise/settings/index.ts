80b35ffc3969a2e8dc8b01c611dd59981742d05b
ColinZeb <Colin@mmin.us>
2023-03-07 16:09:00 +0800

    frontend enterprise

--- enterprise/frontend/src/metabase-enterprise/settings/index.ts
+++ enterprise/frontend/src/metabase-enterprise/settings/index.ts
@@ -1,21 +1,9 @@
-import { hasAnySsoFeature } from "metabase/common/utils/plan";
-import { isTest } from "metabase/env";
 import MetabaseSettings from "metabase/lib/settings";
 import type { TokenFeature } from "metabase-types/api";
 
 export function hasPremiumFeature(feature: TokenFeature) {
-  const hasFeature = MetabaseSettings.get("token-features")?.[feature];
-  if (hasFeature == null) {
-    console.warn(
-      `Unknown premium feature: '${feature}'.`,
-      isTest
-        ? "\nDid you forget to use `mockSettings` instead of `createMockSettings` when testing paid features?"
-        : "",
-    );
-  }
-  return hasFeature;
+  return true;
 }
-
 export function hasAnySsoPremiumFeature() {
-  return hasAnySsoFeature(MetabaseSettings.get("token-features"));
+  return true;
 }
