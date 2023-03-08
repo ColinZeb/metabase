d2e067e8eef5d5754841cbb292651b258d889662
ColinZeb <Colin@mmin.us>
2023-03-08 10:26:41 +0800

    enterprice feature

--- src/metabase/public_settings/premium_features.clj
+++ src/metabase/public_settings/premium_features.clj
@@ -233,7 +233,7 @@
 (defn- has-any-features?
   "True if we have a valid premium features token with ANY features."
   []
-  (boolean (seq (token-features))))
+  (true))
 
 (defn has-feature?
   "Does this instance's premium token have `feature`?
@@ -241,7 +241,7 @@
     (has-feature? :sandboxes)          ; -> true
     (has-feature? :toucan-management)  ; -> false"
   [feature]
-  (contains? (token-features) (name feature)))
+  (true))
 
 (defn- default-premium-feature-getter [feature]
   (fn []
